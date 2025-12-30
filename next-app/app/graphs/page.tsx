df3e88613f429ce8e952a063962a2d0b6aebf09d
carde'a weaver <165233514+Cardeaweaver@users.noreply.github.com>
2025-12-29 22:21:34 -0500

    Add dynamic export to graphs page

--- next-app/app/graphs/page.tsx
+++ next-app/app/graphs/page.tsx
@@ -4,6 +4,8 @@ import React, { Suspense } from 'react';
 import { GraphsPage } from '../../src/components/pages/DataVisualizations/GraphsPage.jsx';
 import { Loading } from '../../src/components/common/Loading.jsx';
 
+export const dynamic = 'force-dynamic';
+
 export default function GraphsRoute() {
   return (
     <Suspense fallback={<Loading />}>
